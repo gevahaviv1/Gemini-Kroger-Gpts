3c5419aaeb365946d2b7d468ed81ba0826929531
Geva Haviv <geva98@gmail.com>
2025-06-19 15:41:30 +0300

    Remove unused code

--- kroger_app/__init__.py
+++ kroger_app/__init__.py
@@ -2,9 +2,8 @@ import os
 import logging
 from flask import Flask, session
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
 
-from .models import db, Product, PriceHistory
+from .models import db
 from .services.kroger_api import (
     fetch_products,
     get_access_token,
@@ -33,12 +32,14 @@ def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///kroger.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Defaults to "dev_secret_key" if not found (secure for dev, not for prod!).
     app.secret_key = os.getenv("FLASK_SECRET_KEY", "dev_secret_key")
     app.config["SESSION_TYPE"] = "filesystem"
     app.config["SESSION_PERMANENT"] = True
     app.config["PERMANENT_SESSION_LIFETIME"] = 1800
     app.config["SESSION_USE_SIGNER"] = True
-    app.config["SESSION_COOKIE_SECURE"] = False
+    app.config["SESSION_COOKIE_SECURE"] = False  # Should be True in production
     app.config["SESSION_COOKIE_HTTPONLY"] = True
     app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
     db.init_app(app)
